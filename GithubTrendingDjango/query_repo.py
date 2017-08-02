0255ad73b38d93a188c4dd5f542ecc2c84599af3
xiyouMc <xiyoumc.dev@gmail.com>
2017-08-02 12:11:26 +0800

    fix Trending

--- GithubTrendingDjango/query_repo.py
+++ GithubTrendingDjango/query_repo.py
@@ -16,17 +16,23 @@ def GithubRepo(request):
         if os.path.exists(settings.dirs + '/' + _get_time() + url_md5):
             with open(settings.dirs + '/' + _get_time() + url_md5, 'r') as f:
                 c = f.readline()
+                if c is not None:
+                    return HttpResponse(c)
         _json = requests.get(github_url, verify=False, headers=settings.header)
-        if (_json):
-            _json_data = json.loads(_json.text)
-            for j in _json_data:
-                print j
-                n = j['name'].lower()
-                print n
-                if n == 'readme.md':
-                    github_url += j['path']
-                    _json = requests.get(github_url, verify=False)
-                    break
+        if 'README.md' in github_url:
+            github_url = github_url.replace('README.md', '')
+            _json = requests.get(github_url, verify=False)
+            print _json.text
+            if (_json):
+                _json_data = json.loads(_json.text)
+                for j in _json_data:
+                    print j
+                    n = j['name'].split('.')[0].lower()
+                    print n
+                    if n == 'readme':
+                        github_url += j['path']
+                        _json = requests.get(github_url, verify=False)
+                        break
         with open(settings.dirs + '/' + _get_time() + url_md5, 'w') as f:
             f.write(_json.text.encode('utf-8'))
         return HttpResponse(_json.text)
