bb3ee03aca0b42e46fed78e927f9e56a08e51fe4
xiyouMc <xiyoumc.dev@gmail.com>
2017-08-03 11:02:39 +0800

    添加 uwsgi 配置

--- GithubTrendingDjango/settings.py
+++ GithubTrendingDjango/settings.py
@@ -138,3 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import django
+django.setup()
+
---
+++ django_wsgi.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+import os
+import sys
+
+# 将系统的编码设置为UTF8
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GithubTrendingDjango.settings")
+
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
