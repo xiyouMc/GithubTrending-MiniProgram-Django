5caa973f58b995e383d6b97844a65f1b856dae17
xiyouMc <xiyoumc.dev@gmail.com>
2017-11-13 22:11:21 +0800

    change port

--- GithubTrendingDjango/_redis.py
+++ GithubTrendingDjango/_redis.py
@@ -7,10 +7,10 @@ class RedisC:
     def __init__(self):
         #     pool = redis.ConnectionPool(host='192.168.129.117', port=6379,password = 'video_2017')
         # pool = redis.ConnectionPool(host=ip, port=6379,password=pwd)
-        pool = redis.ConnectionPool(
-            host='127.0.0.1', port=6379, password=None)
         # pool = redis.ConnectionPool(
         #     host='127.0.0.1', port=6379, password=None)
+        pool = redis.ConnectionPool(
+            host='127.0.0.1', port=6379, password=None)
         self.r = redis.Redis(connection_pool=pool)
 
     def _redis_(self):
