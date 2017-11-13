f713d473ab7a3b69d035e28ea6fc353d07009e7b
xiyouMc <xiyoumc.dev@gmail.com>
2017-11-14 00:30:50 +0800

    fix code

--- GithubTrendingDjango/wx_requests.py
+++ GithubTrendingDjango/wx_requests.py
@@ -38,43 +38,43 @@ def Coupon(request):
                 toUser = recMsg.ToUserName
                 fromUser = recMsg.FromUserName
                 replyMsg = reply.TextMsg(toUser, fromUser, 'avatar_name')
-                resultMsg = replyMsg.send()
+                resultMsg= replyMsg.send()
                 # return HttpResponse(replyMsg.send())
 
                 # if 'instagram.com' in recMsg.Content:
-            # 保存数据库
-            # m = md5.new()
-            # m.update(recMsg.Content)
-            # str_md5 = m.hexdigest()
-            # savedIns = Ins.objects.filter(md5=str_md5).exclude()
-            # if len(savedIns) == 0:
-            #     ins = Ins(md5=str_md5, url=recMsg.Content + '?__a=1')
-            #     ins.save()
+                    # 保存数据库
+                    # m = md5.new()
+                    # m.update(recMsg.Content)
+                    # str_md5 = m.hexdigest()
+                    # savedIns = Ins.objects.filter(md5=str_md5).exclude()
+                    # if len(savedIns) == 0:
+                    #     ins = Ins(md5=str_md5, url=recMsg.Content + '?__a=1')
+                    #     ins.save()
+                    
 
-            # redisData = _get_redis_task(recMsg.Content + '?__a=1')
-            # if redisData is not None:
-            #     resultMsg = userInfo(redisData,toUser,fromUser)
-            #     return HttpResponse(resultMsg)
-            # else:
-            #     _redis_ = _redis.RedisC()
-            #     r = _redis_._redis_()
-            #     r.rpush('ins', recMsg.Content + '?__a=1')
-            #     redisData = None
-            #     while redisData == None:
-            #         redisData = _get_redis_task(
-            #             recMsg.Content + '?__a=1')
-            #         if redisData is not None:
-            #             resultMsg = userInfo(redisData,toUser,fromUser)
-            #             return HttpResponse(resultMsg)
-            #         else:
-            #             resultMsg = "success"
-            #             return HttpResponse(reply.Msg.send())
-            # else:
-            #     print "暂且不处理"
-            #     resultMsg = "success"
-            #     return HttpResponse(reply.Msg.send())
-            :
-            else
+                    # redisData = _get_redis_task(recMsg.Content + '?__a=1')
+                    # if redisData is not None:
+                    #     resultMsg = userInfo(redisData,toUser,fromUser)
+                    #     return HttpResponse(resultMsg)
+                    # else:
+                    #     _redis_ = _redis.RedisC()
+                    #     r = _redis_._redis_()
+                    #     r.rpush('ins', recMsg.Content + '?__a=1')
+                    #     redisData = None
+                    #     while redisData == None:
+                    #         redisData = _get_redis_task(
+                    #             recMsg.Content + '?__a=1')
+                    #         if redisData is not None:
+                    #             resultMsg = userInfo(redisData,toUser,fromUser)
+                    #             return HttpResponse(resultMsg)
+                    #         else:
+                    #             resultMsg = "success"
+                    #             return HttpResponse(reply.Msg.send())
+                # else:
+                #     print "暂且不处理"
+                #     resultMsg = "success"
+                #     return HttpResponse(reply.Msg.send())
+            else:
                 print "暂且不处理"
                 resultMsg = "success"
                 # return HttpResponse(resultMsg)
@@ -86,7 +86,7 @@ def Coupon(request):
         return HttpResponse(json.dumps(a))
 
 
-def userInfo(redisData, toUser, fromUser):
+def userInfo(redisData,toUser,fromUser):
     print redisData
     js = json.loads(redisData)
     avatar_url = js.get('graphql').get('shortcode_media').get('owner').get(
