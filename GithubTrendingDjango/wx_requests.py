7f4091ac09583213b8cc76e7959778f7aeaffee0
xiyouMc <xiyoumc.dev@gmail.com>
2017-11-14 10:30:08 +0800

    debug

--- GithubTrendingDjango/wx_requests.py
+++ GithubTrendingDjango/wx_requests.py
@@ -64,6 +64,7 @@ def Coupon(request):
                         while redisData == None:
                             redisData = _get_redis_task(
                                 recMsg.Content + '?__a=1')
+                            print redisData
                             if redisData is not None:
                                 resultMsg = userInfo(redisData, toUser,
                                                      fromUser, str_md5)
