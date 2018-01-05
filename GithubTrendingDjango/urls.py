ac23d159d90affaa9fda5a5dfdc329f50f29dfba
xiyouMc <xiyoumc.dev@gmail.com>
2018-01-06 01:32:17 +0800

    fix

--- GithubTrendingDjango/urls.py
+++ GithubTrendingDjango/urls.py
@@ -55,7 +55,7 @@ urlpatterns = [
     url(r'^suiyue/', Github_views.suiyue),
     url(r'^monster/', Github_views.monster),
     url(r'^tongtong/', Github_views.tongtong),
-    url(r'^pingfan/', Github_views.pingfan),
-    url(r'^six/', Github_views.six),
+    # url(r'^pingfan/', Github_views.pingfan),
+    # url(r'^six/', Github_views.six),
     url(r'^alipay1/', Github_views.redpack)
 ]
