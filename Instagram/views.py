65f450419e73fe9d1b07f8747ad81738aad5ff00
xiyouMc <xiyoumc.dev@gmail.com>
2018-07-24 00:22:06 +0800

    增加 getOriginData

--- Instagram/views.py
+++ Instagram/views.py
@@ -24,7 +24,7 @@ def imgToBase64(request):
     imgUrl = request.GET['imgUrl']
     imgBase64 = _get_redis_task(imgUrl)
     if imgBase64 is not None:
-        return HttpResponse(imgBase64)
+        return HttpResponse(imgBase64,mimetype="text/plain")
     else:
         _redis_push('imgToBase64', imgUrl)
         imgBase64 = None
@@ -110,6 +110,36 @@ def _redis_push(key, values):
     r = _redis_._redis_()
     return r.rpush(key, values)
 
+
+def getOriginData(request):
+    url = request.GET['url']
+    print url
+    redisData = _get_redis_task(url)
+    isVideo = 'mp4' in url
+    isImage = 'png' or 'jpg' or 'jpeg' in url
+    print redisData
+    if redisData is not None:
+        contentType = ''
+        if isVideo:
+            contentType = 'video/mp4'
+        elif isImage:
+            contentType = 'image/jpeg'
+        return HttpResponse(redisData,content_type=contentType)
+    else:
+        # _write_redis_status('ins',url)
+        r.rpush('ins', url)
+        redisData = None
+        # print  _get_redis_task(url)
+        while redisData == None:
+            redisData = _get_redis_task(url)
+            print redisData
+            if redisData is not None:
+                contentType = ''
+                if isVideo:
+                    contentType = 'video/mp4'
+                elif isImage:
+                    contentType = 'image/jpeg'
+                return HttpResponse(redisData,content_type=contentType)
 
 # Create your views here.
 def q(request):
